6df8657c4494b61dae83772ecc87189cb7fe93f1
Samuel Orji <soi@dixa.com>
2020-04-08 10:24:04 +0200

    excluded example folder

--- src/main/scala/chatroom/Main.scala
+++ src/main/scala/chatroom/Main.scala
@@ -1,10 +1,10 @@
 package chatroom
 
 import akka.NotUsed
-import akka.actor.typed.{ActorSystem, Behavior, Terminated}
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Terminated}
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import chatroom.clients.Client
-import chatroom.clients.Client.Quit
+import chatroom.clients.Client.{ChatRoom, Football, FootballMessage, MasterMessages, PostMessage, Quit}
 import chatroom.master.ChatRoomMaster
 
 object Main {
@@ -13,9 +13,24 @@ object Main {
     //spawn client and master
 
     val chatRoomMaster = ctx.spawn(ChatRoomMaster(),"Master")
-    val client = ctx.spawn(Client(chatRoomMaster),"Client")
+    //val client = ctx.spawn(Client(chatRoomMaster),"Client")
+    val client = new ClientTests(chatRoomMaster,Football,"Samuel",ctx)
 
-    ctx.watch(client)
+
+
+    //Thread.sleep(100)
+
+    client.sendToRoom(FootballMessage("Hello from here"))
+    client.sendToRoom(FootballMessage("Hello from here 2"))
+    client.sendToRoom(FootballMessage("Hello from here 3"))
+    client.sendToRoom(FootballMessage("Hello from here 4 "))
+    client.sendToRoom(FootballMessage("Hello from here 5"))
+
+    client.disconnect
+    client.sendToRoom(FootballMessage("Hello from here 6"))
+
+    //ctx.watch(client)
+
 
     //client ! Quit
 
@@ -34,3 +49,11 @@ object Main {
   }
 
 }
+
+class ClientTests(chatRoomMaster : ActorRef[MasterMessages], connectTo : ChatRoom,screenName : String,context : ActorContext[NotUsed])
+extends Client(chatRoomMaster,connectTo,screenName,context){
+  override def onNotificationReceived(from: String, message: String): Unit =
+    println(s"message receoved from $from, message is $message")
+}
+
+
--- src/main/scala/chatroom/clients/Client.scala
+++ src/main/scala/chatroom/clients/Client.scala
@@ -1,11 +1,11 @@
 package chatroom.clients
 
+import akka.NotUsed
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
+import chatroom.clients.Client.{ChatRoom, ChatRoomMessage, ClientMessages, EndSession, Football, GetSession, MasterMessages, MessagePosted, NotificationReceived, PostMessage, Quit, SessionEnded, SessionGranted, SportMessages, UnProcessedMessage}
 
 object Client {
-
-
   trait ClientMessages
 
   trait ChatRoomMessage
@@ -36,9 +36,11 @@ object Client {
 
 
   case object Quit extends ClientMessages
-  case class FootballMessage(msg: String) extends ChatRoomMessage
 
-  case class BasketBallMessage(msg: String) extends ChatRoomMessage
+  trait SportMessages extends ChatRoomMessage
+  case class FootballMessage(msg: String) extends SportMessages
+
+  case class BasketBallMessage(msg: String) extends SportMessages
 
 
   private def logInfo(msg: String, args: String*)(implicit ctx: ActorContext[ClientMessages]): Unit = {
@@ -76,12 +78,14 @@ object Client {
             println("Quitting Football chat room ")
             chatRoomMaster ! EndSession(pipe,Football)
           }
-          Behaviors.stopped
+          Behaviors.same
 
 
         case SessionEnded =>
           println("Client is done")
-          Behaviors.same
+
+          chatRoomPipe.foreach(_ ! PostMessage(FootballMessage("Hello from this client 7"), ctx.self))
+          Behaviors.stopped
 
 
         case NotificationReceived(from, message) =>
@@ -104,6 +108,83 @@ object Client {
 
     }
 
+}
+
+
+abstract class Client(chatRoomMaster : ActorRef[MasterMessages], connectTo : ChatRoom,screenName : String,context : ActorContext[NotUsed]){
+
+  private [this] var internalActor : Option[ActorRef[ClientMessages]] = Some(context.spawn(props,screenName))
+
+  //encapsulate the actor
+  private def props : Behavior[ClientMessages] = Behaviors.setup { ctx =>
+    chatRoomMaster ! GetSession(screenName, connectTo, ctx.self)
+
+    Behaviors.receiveMessage{
+      case SessionEnded =>
+        Behaviors.stopped
+
+      case SessionGranted(handler) =>
+        println("session granted")
+        roomHandler = Some(handler)
+       Behaviors.same
+
+      case Quit =>
+        disconnectFromRoom
+        Behaviors.same
+
+      case NotificationReceived(from, message) =>
+        onNotificationReceived(from,message)
+        Behaviors.same
+
+      case UnProcessedMessage(message, room) =>
+        ctx.log.info(s"Cannot post {} to {}", message, room.toString)
+        Behaviors.same
+
+      case MessagePosted(msg) =>
+        println(s"msg was posted ")
+        Behaviors.same
+
+    }
+
+
+  }
+
+  final def disconnect : Unit = {
+    roomHandler = None
+    internalActor.foreach(_ ! Quit)
+    internalActor = None
+  }
+
+  def sendToRoom(msg : SportMessages) : Unit = {
+    if(roomHandler.isEmpty){
+      Thread.sleep(50)
+      //should use a future for this instead
+    }
+    if(internalActor.isDefined) {
+      roomHandler.foreach(_ ! PostMessage(msg, internalActor.get))
+    }
+  }
+
+
+  protected def onUnprocessedMessage(msg : String, room : ChatRoom) : Unit  = {
+    println(s"Cannot post {} to {}", msg, room.toString)
+
+  }
+
+  protected def onMessagePosted(msg : String) : Unit  = {
+    println(s"Message Posted", msg)
+
+  }
+
+  private var roomHandler : Option[ActorRef[ChatRoomMessage]] = None
+
+  def onNotificationReceived(from: String, message: String): Unit
+
+  private def disconnectFromRoom = {
+    roomHandler.foreach{ pipe =>
+      chatRoomMaster ! EndSession(pipe,Football)
+    }
+  }
 }
 
 
